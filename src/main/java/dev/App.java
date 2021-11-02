563fd1b4c4125798172a8db114501f2d7c5a55e1
Soliquidus <tibo.pfeifer@gmail.com>
2021-11-02 15:39:34 +0100

    Etape 7

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,13 +1,17 @@
 package dev;
 
+import com.github.lalyos.jfiglet.FigletFont;
+
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException {
+        String asciiArt = FigletFont.convertOneLine( "Hello World!" );
+        System.out.println(asciiArt);
     }
 }
