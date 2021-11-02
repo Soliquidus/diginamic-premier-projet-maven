6ea9fa03e129eb55bbe3481e9f431301b3c8e76e
Soliquidus <tibo.pfeifer@gmail.com>
2021-11-02 15:40:45 +0100

    Etape 7

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class App 
 {
     public static void main( String[] args ) throws IOException {
-        String asciiArt = FigletFont.convertOneLine( "Hello World!" );
+        String asciiArt = FigletFont.convertOneLine( "App" );
         System.out.println(asciiArt);
     }
 }
